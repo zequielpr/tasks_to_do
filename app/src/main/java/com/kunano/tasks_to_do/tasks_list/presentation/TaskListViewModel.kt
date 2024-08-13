809e16824c6ec8e8847c7e9098f1e0f7960017f2
zequielpr <zequielperez.r@gmail.com>
2024-08-13 22:54:04 +0100

    categories

--- app/src/main/java/com/kunano/tasks_to_do/tasks_list/presentation/TaskListViewModel.kt
+++ app/src/main/java/com/kunano/tasks_to_do/tasks_list/presentation/TaskListViewModel.kt
@@ -6,20 +6,50 @@ import androidx.lifecycle.ViewModel
 class TaskListViewModel : ViewModel() {
 
 
+    var testData: MutableLiveData<List<String>> = MutableLiveData(listOf("Task 1", "Task 2"))
 
-    var testData : MutableLiveData<List<String>> = MutableLiveData(listOf("Task 1", "Task 2"))
-
+    var categoriesList: MutableLiveData<List<String>> =
+        MutableLiveData(
+            listOf(
+                "category 1",
+                "Task 2",
+                "category 3",
+                "Task 4",
+                "category 5",
+                "Task 6",
+            )
+        )
 
 
     init {
         val tasksList = arrayListOf<String>()
+        val catList = arrayListOf<String>()
 
-        for(i in 100..200){
+
+        for (i in 100..200) {
             tasksList.add("Task $i")
         }
 
         testData.postValue(tasksList)
 
+        for(i in 5..10){
+            catList.add("category$i")
+        }
+        categoriesList.postValue(catList)
+
+    }
+
+
+    fun selectTaskCategory(category: String){
+
+        val tasksList = arrayListOf<String>()
+
+
+        for (i in 100..200) {
+            tasksList.add("$category: $i")
+        }
+
+        testData.postValue(tasksList)
     }
 
 
--- app/src/main/java/com/kunano/tasks_to_do/tasks_list/presentation/TasksListScreen.kt
+++ app/src/main/java/com/kunano/tasks_to_do/tasks_list/presentation/TasksListScreen.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -19,9 +21,13 @@ import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
@@ -31,20 +37,26 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.AbsoluteAlignment
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import com.kunano.tasks_to_do.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TasksListScreen(viewModel: TaskListViewModel, paddingValues: PaddingValues,
-                    bottomAppBarScrollBehavior: BottomAppBarScrollBehavior) {
+fun TasksListScreen(
+    viewModel: TaskListViewModel,
+    paddingValues: PaddingValues,
+    bottomAppBarScrollBehavior: BottomAppBarScrollBehavior
+) {
 
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
     var floatingButtonExpanded by remember { mutableStateOf(true) }
@@ -53,11 +65,13 @@ fun TasksListScreen(viewModel: TaskListViewModel, paddingValues: PaddingValues,
     floatingButtonExpanded = scrollBehavior.state.heightOffset == 0.0f
 
     Box(modifier = Modifier.fillMaxSize()) {
-        Column(modifier = Modifier
-            .nestedScroll(scrollBehavior.nestedScrollConnection)
-            .nestedScroll(bottomAppBarScrollBehavior.nestedScrollConnection)) {
+        Column(
+            modifier = Modifier
+                .nestedScroll(scrollBehavior.nestedScrollConnection)
+                .nestedScroll(bottomAppBarScrollBehavior.nestedScrollConnection)
+        ) {
 
-            topBar(scrollBehavior = scrollBehavior)
+            topBar(scrollBehavior = scrollBehavior, viewModel)
             taskListContent(paddingValues, viewModel)
         }
 
@@ -68,8 +82,7 @@ fun TasksListScreen(viewModel: TaskListViewModel, paddingValues: PaddingValues,
                 .padding(paddingValues)
                 .padding(20.dp, 50.dp)
         )
-    }
-    /*Scaffold(
+    }/*Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = { topBar(scrollBehavior = scrollBehavior) },
         floatingActionButton = { floatingActionButton(floatingButtonExpanded) },
@@ -80,22 +93,18 @@ fun TasksListScreen(viewModel: TaskListViewModel, paddingValues: PaddingValues,
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun topBar(scrollBehavior: TopAppBarScrollBehavior?) {
+fun topBar(scrollBehavior: TopAppBarScrollBehavior?, viewModel: TaskListViewModel) {
     var dropDownMenuExpanded by remember { mutableStateOf(false) }
     CenterAlignedTopAppBar(
         colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer,
             titleContentColor = MaterialTheme.colorScheme.primary,
         ),
-        title = {
-            Text(
-                "Centered Top App Bar",
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-        },
+        title = {TaskCategoryCarousel(viewModel = viewModel)},
         actions = {
-            IconButton(onClick = { dropDownMenuExpanded = true }) {
+
+            IconButton(
+                onClick = { dropDownMenuExpanded = true }) {
                 Icon(
                     imageVector = Icons.Filled.MoreVert,
                     contentDescription = "Localized description"
@@ -109,20 +118,60 @@ fun topBar(scrollBehavior: TopAppBarScrollBehavior?) {
 
 
 @Composable
+fun TaskCategoryCarousel(viewModel: TaskListViewModel) {
+
+    val context = LocalContext.current
+    val categoriesList by viewModel.categoriesList.observeAsState()
+
+    var selectedCategory by rememberSaveable {
+        mutableStateOf("All")
+    }
+
+    LazyRow(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+    ) {
+        categoriesList?.let { it ->
+            items(it.toList()) { categoryLabel ->
+                categoryBtn(
+                    label = categoryLabel,
+                    selectedCategory = selectedCategory
+                ) { newlySelectedCategory ->
+                    selectedCategory = newlySelectedCategory
+                    viewModel.selectTaskCategory(newlySelectedCategory)
+                }
+            }
+        }
+
+    }
+
+}
+
+@Composable
+fun categoryBtn(
+    label: String,
+    selectedCategory: String,
+    selectCategory: (category: String) -> Unit
+) {
+    FilterChip(
+        shape = ShapeDefaults.Large,
+        selected = label == selectedCategory,
+        onClick = { selectCategory(label) },
+        label = { Text(text = label) })
+}
+
+
+@Composable
 fun dropDownMenu(isExpanded: Boolean, onDismissRequest: () -> Unit) {
 
     DropdownMenu(expanded = isExpanded, onDismissRequest = { onDismissRequest() }) {
-        DropdownMenuItem(
-            text = { Text(text = stringResource(id = R.string.search)) },
+        DropdownMenuItem(text = { Text(text = stringResource(id = R.string.search)) },
             onClick = { /*TODO*/ })
-        DropdownMenuItem(
-            text = { Text(text = stringResource(id = R.string.sort_by)) },
+        DropdownMenuItem(text = { Text(text = stringResource(id = R.string.sort_by)) },
             onClick = { /*TODO*/ })
-        DropdownMenuItem(
-            text = { Text(text = stringResource(id = R.string.manage_categories)) },
+        DropdownMenuItem(text = { Text(text = stringResource(id = R.string.manage_categories)) },
             onClick = { /*TODO*/ })
-        DropdownMenuItem(
-            text = { Text(text = stringResource(id = R.string.delete_task)) },
+        DropdownMenuItem(text = { Text(text = stringResource(id = R.string.delete_task)) },
             onClick = { /*TODO*/ })
     }
 
@@ -144,7 +193,7 @@ fun floatingActionButton(floatingButtonExpanded: Boolean, modifier: Modifier) {
 @Composable
 fun taskListContent(innerPadding: PaddingValues, viewModel: TaskListViewModel) {
 
-    val tasksList = viewModel.testData.observeAsState()
+    val tasksList by viewModel.testData.observeAsState()
 
     LazyColumn(
         modifier = Modifier
@@ -154,7 +203,7 @@ fun taskListContent(innerPadding: PaddingValues, viewModel: TaskListViewModel) {
         verticalArrangement = Arrangement.spacedBy(14.dp)
     ) {
 
-        tasksList?.value?.let {
+        tasksList?.let {
 
             items(it.toList()) { taskName: String ->
                 taskCard(taskName = taskName)
@@ -177,7 +226,7 @@ fun taskCard(taskName: String) {
 @Preview(showBackground = true)
 @Composable
 fun topBarPreview() {
-    topBar(scrollBehavior = null)
+    topBar(scrollBehavior = null, TaskListViewModel())
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
